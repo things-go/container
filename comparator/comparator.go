e4e5146c9cddd43a70a1943d26eb47dbdf8cbdef
thinkgo <thinkgo@aliyun.com>
2021-06-25 17:57:01 +0800

    fix

--- comparator/comparator.go
+++ comparator/comparator.go
@@ -48,106 +48,106 @@ func Compare(v1, v2 interface{}) int {
 		panic(fmt.Sprintf("Two values of different type can't be compared, %s: %s", k1, k2))
 	}
 
-	cmpRet := 0
+	ret := 0
 	switch k1 { // nolint: exhaustive
 	case reflect.Int:
 		cv1, cv2 := v1.(int), v2.(int)
 		if cv1 < cv2 {
-			cmpRet = -1
+			ret = -1
 		} else if cv1 > cv2 {
-			cmpRet = 1
+			ret = 1
 		}
 	case reflect.Uint:
 		cv1, cv2 := v1.(uint), v2.(uint)
 		if cv1 < cv2 {
-			cmpRet = -1
+			ret = -1
 		} else if cv1 > cv2 {
-			cmpRet = 1
+			ret = 1
 		}
 	case reflect.String:
 		cv1, cv2 := v1.(string), v2.(string)
 		if cv1 < cv2 {
-			cmpRet = -1
+			ret = -1
 		} else if cv1 > cv2 {
-			cmpRet = 1
+			ret = 1
 		}
 	case reflect.Float32:
 		cv1, cv2 := v1.(float32), v2.(float32)
 		if cv1 < cv2 {
-			cmpRet = -1
+			ret = -1
 		} else if cv1 > cv2 {
-			cmpRet = 1
+			ret = 1
 		}
 	case reflect.Float64:
 		cv1, cv2 := v1.(float64), v2.(float64)
 		if cv1 < cv2 {
-			cmpRet = -1
+			ret = -1
 		} else if cv1 > cv2 {
-			cmpRet = 1
+			ret = 1
 		}
 	case reflect.Bool:
 		// false < true
 		b1, b2 := v1.(bool), v2.(bool)
 		if !b1 && b2 { // b1 == false && b2 == true
-			cmpRet = -1
+			ret = -1
 		} else if b1 && !b2 { // b1 == true && b2 == false
-			cmpRet = 1
+			ret = 1
 		}
 	case reflect.Int8:
 		cv1, cv2 := v1.(int8), v2.(int8)
 		if cv1 < cv2 {
-			cmpRet = -1
+			ret = -1
 		} else if cv1 > cv2 {
-			cmpRet = 1
-		}
-	case reflect.Int16:
-		cv1, cv2 := v1.(int16), v2.(int16)
-		if cv1 < cv2 {
-			cmpRet = -1
-		} else if cv1 > cv2 {
-			cmpRet = 1
-		}
-	case reflect.Int32: // valid for both int32 and rune
-		cv1, cv2 := v1.(int32), v2.(int32)
-		if cv1 < cv2 {
-			cmpRet = -1
-		} else if cv1 > cv2 {
-			cmpRet = 1
-		}
-	case reflect.Int64:
-		cv1, cv2 := v1.(int64), v2.(int64)
-		if cv1 < cv2 {
-			cmpRet = -1
-		} else if cv1 > cv2 {
-			cmpRet = 1
+			ret = 1
 		}
 	case reflect.Uint8: // valid for both uint8 and byte
 		cv1, cv2 := v1.(uint8), v2.(uint8)
 		if cv1 < cv2 {
-			cmpRet = -1
+			ret = -1
 		} else if cv1 > cv2 {
-			cmpRet = 1
+			ret = 1
+		}
+	case reflect.Int16:
+		cv1, cv2 := v1.(int16), v2.(int16)
+		if cv1 < cv2 {
+			ret = -1
+		} else if cv1 > cv2 {
+			ret = 1
 		}
 	case reflect.Uint16:
 		cv1, cv2 := v1.(uint16), v2.(uint16)
 		if cv1 < cv2 {
-			cmpRet = -1
+			ret = -1
 		} else if cv1 > cv2 {
-			cmpRet = 1
+			ret = 1
+		}
+	case reflect.Int32: // valid for both int32 and rune
+		cv1, cv2 := v1.(int32), v2.(int32)
+		if cv1 < cv2 {
+			ret = -1
+		} else if cv1 > cv2 {
+			ret = 1
 		}
 	case reflect.Uint32:
 		cv1, cv2 := v1.(uint32), v2.(uint32)
 		if cv1 < cv2 {
-			cmpRet = -1
+			ret = -1
 		} else if cv1 > cv2 {
-			cmpRet = 1
+			ret = 1
+		}
+	case reflect.Int64:
+		cv1, cv2 := v1.(int64), v2.(int64)
+		if cv1 < cv2 {
+			ret = -1
+		} else if cv1 > cv2 {
+			ret = 1
 		}
 	case reflect.Uint64:
 		cv1, cv2 := v1.(uint64), v2.(uint64)
 		if cv1 < cv2 {
-			cmpRet = -1
+			ret = -1
 		} else if cv1 > cv2 {
-			cmpRet = 1
+			ret = 1
 		}
 	case reflect.Struct:
 		// compare time
@@ -159,8 +159,7 @@ func Compare(v1, v2 interface{}) int {
 	default:
 		panic(fmt.Sprintf("type '%s' can't be compared,please define a customized comparator.Comparator for your struct", k1))
 	}
-
-	return cmpRet
+	return ret
 }
 
 // CompareTime compares its two arguments if both of them are time.Time, and returns true
